146bacedc181190c821479f37b5c32df4e277cb6
Hillary Neaves <hillaryn23@ou.edu>
2021-02-12 14:26:37 -0600

    WPF1 Participation commit

---
+++ WPF1/WPF1/EntryForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF1
+{
+    public class EntryForm
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}" +
+                $"\rAddress: {Address}" +
+                $"\rZipCode: {ZipCode}";
+        }
+    }
+}
