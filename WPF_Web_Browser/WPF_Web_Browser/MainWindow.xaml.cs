ae4eeb2d21e9d4f68fe4a3d2fda3f25fc17d9702
Hillary Neaves <hillaryn23@ou.edu>
2021-02-18 14:26:37 -0600

    WPF Web App in-class participation

---
+++ WPF_Web_Browser/WPF_Web_Browser/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Web_Browser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            string URL = txtURL.Text;
+
+            //webGoogle.Navigate($"https://www.google.com/search?q={URL}";
+            MyWebBrowserWindow google = new MyWebBrowserWindow();
+            google.Query = URL;
+            google.Search();
+            google.Search(URL);
+            google.ShowDialog();
+
+        }
+    }
+}
