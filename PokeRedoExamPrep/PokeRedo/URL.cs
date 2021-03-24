48d55615518abb993709bb0fb45cbb546367ab77
Hillary Neaves <hillaryn23@ou.edu>
2021-03-24 18:14:54 -0500

    completed the redo of Pokemon participation

---
+++ PokeRedoExamPrep/PokeRedo/URL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeRedo
+{
+    public class URL
+    {
+        public string height { get; set; }
+        public string weight { get; set; }
+        public Sprite sprites { get; set; }
+    }
+
+    public class Sprite
+    {
+        public string front_default { get; set; }
+        public string back_default { get; set; }
+    }
+}
